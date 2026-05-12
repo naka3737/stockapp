52e972634af5a08994900355a42d1a56b05dc146
naka3737 <dknk1515@gmail.com>
2026-05-12 17:43:26 +0900

    Add files via upload

---
+++ pages/fudousan.py
@@ -0,0 +1,47 @@
+import streamlit as st
+import pandas as pd
+import yfinance as yf
+from japaname import get_data
+
+def main():
+
+    st.set_page_config(page_title="不動産株", menu_items=None)
+    st.title("不動産株")
+
+    # 調べたい銘柄リスト
+
+    codes = ["8801.T","8802.T","8830.T","3231.T", "3289.T", "8803.T","8934.T","3003.T"]
+
+    with st.spinner('データを取得中...'):
+
+        # yf.downloadで一括取得（高速化）
+        df = yf.download(codes, period="2d")
+
+        # 画面を横に分割（銘柄ごとにカードを並べる）
+        #cols = st.columns(len(codes))
+
+        for i, code in enumerate(codes):
+
+            jname = get_data(code.replace(".T", ""))
+            prev_close = df['Close'][code].iloc[0]
+            last_price = df['Close'][code].iloc[-1]
+            change_ratio = ((last_price - prev_close) / prev_close) * 100
+            
+            # ブラウザ専用の綺麗な表示（ラベル, 現在値, 前日比）
+            st.metric(
+                label=f"{code} {jname}",
+                value=f"{last_price:.1f} 円",
+                delta=f"{change_ratio:.2f}%"
+            )
+
+            
+        print(df.tail())
+
+    # 下に詳細な表も表示してみる
+    st.subheader("詳細データ")
+    st.dataframe(df['Close'].tail())
+
+main()
+
+if st.button("戻る"):
+    st.switch_page("mainpage.py")
